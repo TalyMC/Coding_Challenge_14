d27026a5eafc6f6038c8c476904214c80b1b4dfc
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-03-09 23:55:52 -0400

    Task 3 - Highlighting High Priority Tickets

--- cc_14.js
+++ cc_14.js
@@ -1,20 +1,26 @@
 //Task 2
 function ticketCard(customerName, IssueDescription, priorityLevel) {
     const ticketContainer = document.getElementById('ticketContainer');
+    
+    //Div for Tickets
     const card = document.createElement('div');
     card.setAttribute('class', 'ticket');
     ticketContainer.appendChild(card);
 
+    //Heading for Cutomer Name
     const heading = document.createElement('h2');
     heading.textContent = customerName;
 
+    //SubHeading for Issue
     const itHasIssues = document.createElement('h4');
     itHasIssues.textContent = IssueDescription;
 
+    //Priority Level
     const prioritycond = document.createElement('span');
     prioritycond.textContent = `Priority: ${priorityLevel}`;
     prioritycond.setAttribute('class', priorityLevel.toLowerCase()); // Apply 'high' or 'low' class
 
+    //Resolve Button
     const resolveButton = document.createElement('button');
     resolveButton.textContent = 'Edit';
     resolveButton.setAttribute('class', 'edit-button');
@@ -25,7 +31,19 @@ function ticketCard(customerName, IssueDescription, priorityLevel) {
     card.appendChild(prioritycond);
     card.appendChild(resolveButton);
 };
-
 // Example Tickets
 ticketCard('Benito B.B.', 'Needs Maintenance ASAP', 'High');
 ticketCard('Rauw A.', 'Page Responsive - Slow', 'Low');
+
+//Task 3
+function highPriortyHighlight() {
+    const highPriortyHighlight = document.querySelectorAll('.high');
+    const highProArray = Array.from(highPriortyHighlight);
+
+    highProArray.forEach(ticket => {
+        ticket.style.backgroundColor = 'rgb(225, 97, 97)';
+        ticket.style.border = '2px solid black'
+    });
+};
+
+highlightHighPriorityTickets()
